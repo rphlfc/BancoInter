a4637d0399353d8ca7843812bb8f1e85b846d539
Raphael Cerqueira <rcerqueira@vr.com.br>
2020-12-23 20:18:57 -0300

    Initial Commit

---
+++ BancoInter/BancoInterApp.swift
@@ -0,0 +1,17 @@
+//
+//  BancoInterApp.swift
+//  BancoInter
+//
+//  Created by Raphael Cerqueira on 23/12/20.
+//
+
+import SwiftUI
+
+@main
+struct BancoInterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BancoInter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BancoInter
+//
+//  Created by Raphael Cerqueira on 23/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
